28082fc93ca41b76bb1b1c40b1a12f7ed3eeee12
user130165 <user130165@ny211.local>
2017-10-13 10:09:50 -0700

    Initial Commit

---
+++ ImagenesProyecto3/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ImagenesProyecto3
+//
+//  Created by user130165 on 10/13/17.
+//  Copyright © 2017 KevinGonzalez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ ImagenesProyecto3UITests/ImagenesProyecto3UITests.swift
@@ -0,0 +1,36 @@
+//
+//  ImagenesProyecto3UITests.swift
+//  ImagenesProyecto3UITests
+//
+//  Created by user130165 on 10/13/17.
+//  Copyright © 2017 KevinGonzalez. All rights reserved.
+//
+
+import XCTest
+
+class ImagenesProyecto3UITests: XCTestCase {
+        
+    override func setUp() {
+        super.setUp()
+        
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        
+        // In UI tests it is usually best to stop immediately when a failure occurs.
+        continueAfterFailure = false
+        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
+        XCUIApplication().launch()
+
+        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // Use recording to get started writing UI tests.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+}
