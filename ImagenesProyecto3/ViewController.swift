a18bc223dd95907a2291a1a8581c20a56327c29c
Kevin Alexandro <kevin007pln@gmail.com>
2017-12-22 17:33:05 -0500

    Proyecto 3 IOS USFQ 2017
    Galería de imagenes descargadas de internet by KG.

--- ImagenesProyecto3/ViewController.swift
+++ ImagenesProyecto3/ViewController.swift
@@ -9,17 +9,80 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    
+    //Lista de imagenes
+    let allImagenes =
+    [
+        "https://www.nasa.gov/sites/default/files/thumbnails/image/potw1737a.jpg",
+        "https://www.nasa.gov/sites/default/files/thumbnails/image/potw1636a.jpg",
+        "https://www.nasa.gov/sites/default/files/thumbnails/image/hinode_aug21.png",
+        "https://www.nasa.gov/sites/default/files/thumbnails/image/name_chip_main-opt.jpg",
+        "https://www.nasa.gov/sites/default/files/thumbnails/image/westerlund_2_composite.jpg",
+        "https://www.nasa.gov/sites/default/files/thumbnails/image/cdfs2_0.jpg",
+        "https://www.nasa.gov/sites/default/files/thumbnails/image/cen_multi.jpg",
+        "https://www.nasa.gov/sites/default/files/thumbnails/image/arp299.jpg",
+        "https://www.nasa.gov/sites/default/files/thumbnails/image/potw1722a.jpg",
+        "https://www.nasa.gov/sites/default/files/thumbnails/image/eclipsecorona.png"
+    ]
+    
+    
+    //accion que es adoptada por todos los botones.
+    
+    @IBAction func CambiarImagen(_ sender: UIButton) {
+        //se crea un hilo para cambiar de imagen.
+        
+        DispatchQueue.global(qos: .userInitiated).async
+        {
+            self.showImage(button: sender)
+        }
     }
+    
+    
+    var indexImagenesEnUso = [11,11,11,11,11,11]; //se incializa con valores que nunca van a ser alcanzadas
+    
+    //cambiar las imagenes
+    func showImage(button : UIButton)
+    {
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        //creo y cargo la imagen
+        var imagen : UIImage
+        {
+            get
+            {
+                return button.currentImage!
+            }
+            set
+            {
+                button.setImage(newValue, for: .normal)
+            }
+        }
+        
+        var regresar = false
+        var index = 0
+        
+        repeat
+        {
+            regresar = false
+            let loteria = Int (arc4random_uniform(10))
+            index = loteria
+            if indexImagenesEnUso.contains(loteria)
+            {
+                regresar = true
+            }
+        }
+        while regresar
+        
+        indexImagenesEnUso[button.tag] = index
+        if let url = URL.init(string: allImagenes[index])
+        {
+            let urlLink = try? Data(contentsOf: url)
+            if let imageData = urlLink
+            {
+                imagen = UIImage(data: imageData)!
+            }
+        }
+        
     }
-
-
+    
 }
 
